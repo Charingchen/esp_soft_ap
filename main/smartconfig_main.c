7f7659a16d77dd5aae641431525c21066f2490c7
Charing Chen <charinghaxen@gmail.com>
2020-12-08 23:00:48 -0800

    feat: Add ping function
    bug: seems like the wifi switch over is still not done correctly. Need to modifiy event handler more to see if that fix it
    bug: The Ecplise build still throwing errors, need to reconfigure the project later

--- main/smartconfig_main.c
+++ main/smartconfig_main.c
@@ -22,6 +22,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "ping/ping_sock.h"
+#include "ping/ping.h"
+
 
 #define SSID "ESP32AP"
 //#define customIp ((u32_t)0xC0A80103UL) //102.168.1.3
@@ -183,6 +186,110 @@ void print_sta_info(void *pvParam){
 	}
 }
 
+/*
+ * ESP32 ICMP Echo
+ * https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/protocols/icmp_echo.html
+ */
+static void cmd_ping_on_ping_success(esp_ping_handle_t hdl, void *args)
+{
+    uint8_t ttl;
+    uint16_t seqno;
+    uint32_t elapsed_time, recv_len;
+    ip_addr_t target_addr;
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_TTL, &ttl, sizeof(ttl));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SIZE, &recv_len, sizeof(recv_len));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_TIMEGAP, &elapsed_time, sizeof(elapsed_time));
+    printf("%d bytes from %s icmp_seq=%d ttl=%d time=%d ms\n",
+           recv_len, ipaddr_ntoa((ip_addr_t*)&target_addr), seqno, ttl, elapsed_time);
+}
+
+static void cmd_ping_on_ping_timeout(esp_ping_handle_t hdl, void *args)
+{
+    uint16_t seqno;
+    ip_addr_t target_addr;
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    printf("From %s icmp_seq=%d timeout\n",ipaddr_ntoa((ip_addr_t*)&target_addr), seqno);
+}
+
+static void cmd_ping_on_ping_end(esp_ping_handle_t hdl, void *args)
+{
+    ip_addr_t target_addr;
+    uint32_t transmitted;
+    uint32_t received;
+    uint32_t total_time_ms;
+    esp_ping_get_profile(hdl, ESP_PING_PROF_REQUEST, &transmitted, sizeof(transmitted));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_REPLY, &received, sizeof(received));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_DURATION, &total_time_ms, sizeof(total_time_ms));
+    uint32_t loss = (uint32_t)((1 - ((float)received) / transmitted) * 100);
+    if (IP_IS_V4(&target_addr)) {
+        printf("\n--- %s ping statistics ---\n", inet_ntoa(*ip_2_ip4(&target_addr)));
+    } else {
+        printf("\n--- %s ping statistics ---\n", inet6_ntoa(*ip_2_ip6(&target_addr)));
+    }
+    printf("%d packets transmitted, %d received, %d%% packet loss, time %dms\n",
+           transmitted, received, loss, total_time_ms);
+    // delete the ping sessions, so that we clean up all resources and can create a new ping session
+    // we don't have to call delete function in the callback, instead we can call delete function from other tasks
+    esp_ping_delete_session(hdl);
+}
+
+
+int ping_start()
+{
+	esp_ping_config_t config = ESP_PING_DEFAULT_CONFIG();
+
+
+	// parse IP address
+	struct sockaddr_in6 sock_addr6;
+	ip_addr_t target_addr;
+	memset(&target_addr, 0, sizeof(target_addr));
+	char *ping_target = "www.espressif.com";
+
+	if (inet_pton(AF_INET6, ping_target, &sock_addr6.sin6_addr) == 1) {
+		/* convert ip6 string to ip6 address */
+		ipaddr_aton(ping_target, &target_addr);
+	} else {
+		struct addrinfo hint;
+		struct addrinfo *res = NULL;
+		memset(&hint, 0, sizeof(hint));
+		/* convert ip4 string or hostname to ip4 or ip6 address */
+		if (getaddrinfo(ping_target, NULL, &hint, &res) != 0) {
+			printf("ping: unknown host %s\n", ping_target);
+			return 1;
+		}
+		if (res->ai_family == AF_INET) {
+			struct in_addr addr4 = ((struct sockaddr_in *) (res->ai_addr))->sin_addr;
+			inet_addr_to_ip4addr(ip_2_ip4(&target_addr), &addr4);
+		} else {
+			struct in6_addr addr6 = ((struct sockaddr_in6 *) (res->ai_addr))->sin6_addr;
+			inet6_addr_to_ip6addr(ip_2_ip6(&target_addr), &addr6);
+		}
+		freeaddrinfo(res);
+	}
+	config.target_addr = target_addr;
+
+	/* set callback functions */
+	esp_ping_callbacks_t cbs = {
+		.on_ping_success = cmd_ping_on_ping_success,
+		.on_ping_timeout = cmd_ping_on_ping_timeout,
+		.on_ping_end = cmd_ping_on_ping_end,
+		.cb_args = NULL
+	};
+	esp_ping_handle_t ping;
+	esp_ping_new_session(&config, &cbs, &ping);
+
+    ESP_LOGI(TAG, "Ping start");
+    ESP_ERROR_CHECK(esp_ping_start(ping));
+    ESP_LOGI(TAG, "Ping end");
+    return 0;
+
+}
+
+
 int cmd_detection(const char* input) {
 //	static unsigned int input_len;
 //	input_len = sizeof(input);
@@ -239,8 +346,10 @@ int cmd_detection(const char* input) {
 			ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
 			ESP_ERROR_CHECK( esp_wifi_connect() );
 
-		    ESP_LOGI(TAG, "Waiting for wifi");
-		    vTaskDelete( xtcp_server_handle );
+		    ESP_LOGI(TAG, "Testing connection to ping espressif.com");
+		    //Try to ping espressif.com
+		    ping_start();
+//		    vTaskDelete( xtcp_server_handle );
 			break;
 
 		case EN_TX:
@@ -271,12 +380,7 @@ int cmd_detection(const char* input) {
 				   sprintf(tempstring,"SSID:%s.RSSI:%3d.Authmode: %s.\n",list[i].ssid, list[i].rssi, authmode);
 				   printf(tempstring);
 				   strcat(info_tosend, tempstring);
-//				   if (i == 0){
-//					   strcpy(info_tosend, tempstring);
-//				   }
-//				   else{
-//					   strcat(info_tosend, tempstring);
-//				   }
+
 				}
 				printf("info_tosend:\n");
 				printf(info_tosend);
@@ -414,6 +518,9 @@ void tcp_server(void *pvParam){
     }
     ESP_LOGI(TAG, "...tcp_client task closed\n");
 }
+
+
+
 
 void app_main(void)
 {
